0eecf980c31935ae5699e4425365624f169d2654
talhahassan0005 <163239430+talhahassan0005@users.noreply.github.com>
2024-06-24 18:29:09 +0500

    Add files via upload
    Create a file named 404.js inside the pages directory.
    Export a React component that serves as your custom 404 page.
    Customize the component to provide relevant information or navigation options for users encountering a page not found error in your Next.js application.

---
+++ Page404.js
@@ -0,0 +1,9 @@
+export default function PageNotFoundError()
+{
+    return(
+
+        <div>
+            <h1>Sorry!</h1><br></br><h2>This page is currently available</h2>
+        </div>
+    )
+};
