9687bf96a9abd10ab48b929dfe81527faced0802
talhahassan0005 <163239430+talhahassan0005@users.noreply.github.com>
2024-07-08 16:31:10 +0500

    Create Page.js

---
+++ REdirection-In_Next.js/Page.js
@@ -0,0 +1,19 @@
+
+
+export default function Home() {
+
+  return (
+    <div >
+      <p > Redirection in Next.JS.<br></br><br></br>
+        when you put the path Like(localhost3000/user/1234) in url bar it willl direct to the Main or home Page.<br></br>
+        The code of redirection iw written in next.config.js file.
+      </p>
+
+
+
+    </div>
+  );
+}
+
+
+
